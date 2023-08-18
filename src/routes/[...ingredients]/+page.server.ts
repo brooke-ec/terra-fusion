05d334b2e2129f24f21f481b46971b44a62e46c8
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-08-18 16:05:21 +0100

    feat: fetch fusion result

--- src/routes/[...ingredients]/+page.server.ts
+++ src/routes/[...ingredients]/+page.server.ts
@@ -1,3 +1,4 @@
+import type { Character } from "$lib/types/db.js";
 import { error } from "@sveltejs/kit";
 import { pg } from "$lib/server/db";
 
@@ -7,12 +8,17 @@ export async function load({ params }) {
 	const ingredients = await pg("character").whereIn("name", ingredientNames);
 
 	if (ingredientNames.some((i) => !ingredients.find((x) => x.name == i))) {
-		throw error(404, "Not found");
+		throw error(404, "Not Found");
 	}
+
+	const ingredientIds = ingredients.map((i) => i.id);
+	const results = await pg<Character>(pg.raw("fusion_result(?)", [ingredientIds]));
+	const result = results.length > 0 ? results[0] : null;
 
 	return {
 		ingredients: ingredients.sort(
 			(a, b) => ingredientNames.indexOf(a.name) - ingredientNames.indexOf(b.name),
 		),
+		result: result,
 	};
 }
