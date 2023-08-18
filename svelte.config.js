6b8e8db7f79eb96033b44d9f90ba3ca31b99229e
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-08-18 23:27:08 +0100

    chore: use cloudflare adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import sequence from "svelte-sequential-preprocessor";
 import { vitePreprocess } from "@sveltejs/kit/vite";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { preprocessMeltUI } from "@melt-ui/pp";
-import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +13,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({}),
 	},
 };
 
