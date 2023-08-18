a9bcf55fed94b7e64169912093590f9334e59889
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-08-18 12:29:51 +0100

    feat: add basic fusion page

---
+++ src/routes/[...characters]/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
+	import Fa from "svelte-fa";
+</script>
+
+<a class="clear" href="/">
+	<Fa icon={faTrashCan} /> Clear
+</a>
+
+<div class="container" />
+
+<style lang="scss">
+	.clear {
+		color: var(--accent-red-10);
+
+		&:hover {
+			color: var(--accent-red-9);
+		}
+
+		&:active {
+			color: var(--accent-red-8);
+		}
+	}
+
+	.container {
+		background-color: var(--background-9);
+		border-radius: 10px;
+		min-height: 400px;
+		margin: 10px 0;
+	}
+</style>
