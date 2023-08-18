04d2a8d5cad0e03817d99789a0b87dc8c79b7b33
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-08-18 15:37:32 +0100

    fix: vite-imagetools typing error
    Co-authored-by: Willow (GHOST) <ghostdevv@users.noreply.github.com>

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+declare module "*&format=webp" {
+	const value: string;
+	export default value;
+}
+
+declare module "*?format=webp" {
+	const value: string;
+	export default value;
+}
