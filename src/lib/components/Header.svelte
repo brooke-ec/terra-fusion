5cef8e8e692fabb8ab82b74f15f2f5c972d2428e
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-08-20 11:29:52 +0100

    feat: add loading animation

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import logo from "$lib/assets/logo.png?format=webp";
+	import { navigating } from "$app/stores";
 </script>
 
 <header>
 	<img src={logo} alt="Terra Fusion Logo" height="60px" />
+	{#if $navigating}
+		<div class="loading-bar" />
+	{/if}
 </header>
 
 <style lang="scss">
@@ -15,5 +19,33 @@
 		height: 80px;
 		width: 100%;
 		z-index: 10;
+	}
+
+	.loading-bar {
+		height: 2px;
+		width: 100vw;
+		margin-top: 4px;
+		position: absolute;
+		margin-left: -10px;
+		background: repeating-linear-gradient(
+			to right,
+			var(--accent-red-8) 0%,
+			var(--accent-red-10) 20%,
+			var(--accent-red-8)
+		);
+		background-size: 200% auto;
+		background-position: 0 100%;
+		animation: gradient 1s infinite;
+		animation-fill-mode: forwards;
+		animation-timing-function: linear;
+	}
+
+	@keyframes gradient {
+		0% {
+			background-position: 0 0;
+		}
+		100% {
+			background-position: -200% 0;
+		}
 	}
 </style>
